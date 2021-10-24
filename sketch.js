8dc389f30037621ced59c7408a31c687703e73bc
sanjeevaattygallethecoolkid <92202783+sanjeevaattygallethecoolkid@users.noreply.github.com>
2021-10-24 17:44:48 +1100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -79,8 +79,8 @@ function draw() {
       if(swordGroup.isTouching(boy)) {
         gameState=END;
         
-        boy.addAnimation(endImg);
-        // boy.addAnimation("SahilRunning",endImg);
+        // boy.addAnimation(endImg);
+        boy.addAnimation("SahilRunning",endImg);
         // boy.addAnimation("SahilRunning");
         // boy.addAnimation(SahilRunning,endImg);
 
@@ -88,20 +88,20 @@ function draw() {
         boy.y=300;
         boy.scale=0.6;
         
-        cashG.destroyEach;
-        diamondsG.destroyEach;
-        jewelryG.destroyEach;
-        swordGroup.destroyEach;
+        // cashG.destroyEach;
+        // diamondsG.destroyEach;
+        // jewelryG.destroyEach;
+        // swordGroup.destroyEach;
 
         // cashG.destroy();
         // diamondsG.destroy();
         // jewelryG.destroy();
         // swordGroup.destroy();
         
-        // cashG.destroyEach();
-        // diamondsG.destroyEach();
-        // jewelryG.destroyEach();
-        // swordGroup.destroyEach();
+        cashG.destroyEach();
+        diamondsG.destroyEach();
+        jewelryG.destroyEach();
+        swordGroup.destroyEach();
         
         // cashGdestroyEach();
         // diamondsGdestroyEach();
